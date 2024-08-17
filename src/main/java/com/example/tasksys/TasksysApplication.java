e97712f454184b08a7b3e7a2f7e3df061547764d
dannrick <daniel.ricardo.souza@gmail.com>
2024-08-17 19:46:23 -0300

    Correção do TasksysApplication.java

--- src/main/java/com/example/tasksys/TasksysApplication.java
+++ src/main/java/com/example/tasksys/TasksysApplication.java
@@ -2,12 +2,14 @@ package com.example.tasksys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.tasksys.model")
+@EnableJpaRepositories(basePackages = "com.example.tasksys.repository")
 public class TasksysApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TasksysApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TasksysApplication.class, args);
+    }
 }
