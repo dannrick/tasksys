dcc8ff1afbadb28dac0a70c2a66ebdee6be1c432
dannrick <daniel.ricardo.souza@gmail.com>
2024-08-17 19:45:37 -0300

    Adaptação do TaskController para fazer o CRUD com o banco de dados

--- src/main/java/com/example/tasksys/controller/TaskController.java
+++ src/main/java/com/example/tasksys/controller/TaskController.java
@@ -1,34 +1,35 @@
 package com.example.tasksys.controller;
 
 import com.example.tasksys.model.Tarefa;
+import com.example.tasksys.repository.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class TaskController {
-    private List<Tarefa> tarefas = new ArrayList<>();
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
 
     @GetMapping("/")
     public String index(@RequestParam(value = "status", required = false) String status, Model model) {
-        List<Tarefa> tarefasFiltradas = new ArrayList<>(tarefas);
+        List<Tarefa> tarefasFiltradas;
         
         if (status != null && !status.isEmpty()) {
-            tarefasFiltradas = tarefas.stream()
-                .filter(tarefa -> tarefa.getStatus().equals(status))
-                .collect(Collectors.toList());
+            tarefasFiltradas = tarefaRepository.findByStatus(status);
+        } else {
+            tarefasFiltradas = tarefaRepository.findAll();
         }
 
         model.addAttribute("tarefas", tarefasFiltradas);
         return "index";
     }
-
 
     @GetMapping("/cadastro")
     public String cadastro() {
@@ -38,53 +39,51 @@ public class TaskController {
     @PostMapping("/cadastro")
     public String cadastrarTarefa(@RequestParam String titulo, @RequestParam String descricao) {
         Tarefa tarefa = new Tarefa();
-        tarefa.setId((long) (tarefas.size() + 1));
         tarefa.setTitulo(titulo);
         tarefa.setDescricao(descricao);
         tarefa.setStatus("Sem status definido"); // Define o status padrão
-        tarefas.add(tarefa);
+        tarefaRepository.save(tarefa);
         return "redirect:/";
     }
 
-
     @GetMapping("/detalhe")
     public String detalhe(@RequestParam Long id, Model model) {
-        Tarefa tarefa = tarefas.stream().filter(f -> f.getId().equals(id)).findFirst().orElse(null);
+        Tarefa tarefa = tarefaRepository.findById(id).orElse(null);
         model.addAttribute("tarefa", tarefa);
         return "detalhe";
     }
-    
+
     @PostMapping("/atualizar-status")
     public String atualizarStatus(@RequestParam Long id, @RequestParam String status) {
-        Tarefa tarefa = tarefas.stream().filter(f -> f.getId().equals(id)).findFirst().orElse(null);
+        Tarefa tarefa = tarefaRepository.findById(id).orElse(null);
         if (tarefa != null) {
             tarefa.setStatus(status);
+            tarefaRepository.save(tarefa);
         }
         return "redirect:/";
     }
 
     @GetMapping("/editar")
     public String editar(@RequestParam Long id, Model model) {
-        Tarefa tarefa = tarefas.stream().filter(f -> f.getId().equals(id)).findFirst().orElse(null);
+        Tarefa tarefa = tarefaRepository.findById(id).orElse(null);
         model.addAttribute("tarefa", tarefa);
         return "editar";
     }
 
     @PostMapping("/atualizar-tarefa")
     public String atualizarTarefa(@RequestParam Long id, @RequestParam String titulo, @RequestParam String descricao) {
-        Tarefa tarefa = tarefas.stream().filter(f -> f.getId().equals(id)).findFirst().orElse(null);
+        Tarefa tarefa = tarefaRepository.findById(id).orElse(null);
         if (tarefa != null) {
             tarefa.setTitulo(titulo);
             tarefa.setDescricao(descricao);
+            tarefaRepository.save(tarefa);
         }
         return "redirect:/";
     }
-    
+
     @PostMapping("/excluir-tarefa")
     public String excluirTarefa(@RequestParam Long id) {
-        tarefas.removeIf(tarefa -> tarefa.getId().equals(id));
+        tarefaRepository.deleteById(id);
         return "redirect:/";
     }
-
-
 }
